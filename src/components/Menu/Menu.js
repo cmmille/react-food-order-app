dcea4642c240f3103072c1634f0df134caaacff6
cmmille <cmmiller.bio@gmail.com>
2022-05-10 22:40:22 -0600

    added http menu and minor ui improvements

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,35 +1,37 @@
-import { uid } from "uid";
+import { useContext, useEffect, useState } from "react";
+
+import useHttp from "../../hooks/use-http";
 
 import Card from "../UI/Card/Card";
 import MenuItem from "./MenuItem/MenuItem";
 import CartContext from "../../store/cart-context"
 
 import styles from "./Menu.module.css";
-import { useContext } from "react";
-
-const dummyItems = [
-  {
-    name: "Mariner's Select",
-    description: "A fine blend of feesh chunks. Selected just for you!",
-    price: 19.99,
-    key: uid(),
-  },
-  {
-    name: "Stinky Stink Feesh",
-    description: "It's really stinky.",
-    price: 5.99,
-    key: uid(),
-  },
-  {
-    name: "Salmon Dinner",
-    description: "Pacific pink feesh.",
-    price: 12.69,
-    key: uid(),
-  },
-];
 
 const Menu = () => {
 const cartCtx = useContext(CartContext)
+const {error, isLoading, sendRequest: getMenuItems} = useHttp()
+const [menuItems, setMenuItems] = useState([])
+
+useEffect(() => {
+  function transformData(data){
+    const loadedMenuItems = []
+    for (const menuKey in data) {
+      loadedMenuItems.push({
+        key: menuKey,
+        name: data[menuKey].name,
+        description: data[menuKey].description,
+        price: data[menuKey].price
+      })
+    }
+    setMenuItems(loadedMenuItems)
+  }
+
+  getMenuItems({
+    url: "https://react-course-80b51-default-rtdb.firebaseio.com/menus.json"
+  }, transformData)
+}, [getMenuItems])
+
 
 function submitHandler(item) {
   cartCtx.onAddToCart(item)
@@ -37,7 +39,9 @@ function submitHandler(item) {
 
   return (
     <Card className={styles.menu}>
-      {dummyItems.map((item) => {
+    {error && <p>Menu not found. Please try again later.</p>}
+    {isLoading && <p>Loading...</p>}
+      {menuItems.map((item) => {
         return (
           <MenuItem
             id={item.key}
