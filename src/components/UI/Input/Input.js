902587821b250f8f2fc2297ee66ebb1ef0417984
cmmille <cmmiller.bio@gmail.com>
2022-05-11 23:44:48 -0600

    added checkout form

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import styles from "./Input.module.css";
+
+const Input = (props) => {
+  // State
+//   const [isValid, setIsValid] = useState(false);
+  const [isTouched, setIsTouched] = useState(false);
+  const isInvalid = !props.isValid & isTouched;
+
+  // Handlers
+  function changeHandler(event) {
+    props.onChange(event);
+  }
+  function blurHandler() {
+    setIsTouched(true);
+  }
+
+  // Components
+  const inputClassNames = `${styles.input} ${isInvalid && styles.invalid}`;
+  return (
+    <>
+      <label htmlFor="name" className={styles["form-label"]}>
+        {props.label}
+      </label>
+      <input
+        name={props.name}
+        value={props.value}
+        onChange={changeHandler}
+        onBlur={blurHandler}
+        className={inputClassNames}
+        type="text"
+      />
+      {isInvalid ? (
+        <p className={styles["invalid-label"]}>{props.label} cannot be blank.</p>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+export default Input;
