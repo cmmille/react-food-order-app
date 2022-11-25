d8aa0f4b87bd1f12bdd06cf55af610d78d7bfc60
cmmille <cmmiller.bio@gmail.com>
2022-11-25 12:25:02 -0800

    Update dependencies

--- src/components/Navbar/TinyCart/TinyCart.js
+++ src/components/Navbar/TinyCart/TinyCart.js
@@ -1,5 +1,5 @@
 import Button from "../../UI/Button/Button";
-import { ShoppingCart } from "@mui/icons-material";
+import { HiShoppingCart } from "react-icons/hi";
 import NotificationBubble from "../../UI/NotificationBubble/NotificationBubble";
 
 import styles from "./TinyCart.module.css";
@@ -9,17 +9,13 @@ import CartContext from "../../../store/cart-context";
 const TinyCart = () => {
   const cartContext = useContext(CartContext);
 
-  function clickHandler(){
-      cartContext.onCart()
+  function clickHandler() {
+    cartContext.onCart();
   }
 
   return (
-    <Button 
-    className={styles.cart} 
-    styled="dark" 
-    onClick={clickHandler}>
-
-      <ShoppingCart />
+    <Button className={styles.cart} styled="dark" onClick={clickHandler}>
+      <HiShoppingCart />
       <h2>Your Cart</h2>
       <NotificationBubble count={cartContext.itemsInCart} />
     </Button>
